7964f32a6b40a389f8f1dc266babef93b7b9c5f2
Vitaly Rtishchev <rtivital@gmail.com>
2024-05-02 10:42:19 +0400

    Add example with emotion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "@mantine/core/styles.css";
-import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
+import { RootStyleRegistry } from "./EmotionRootStyleRegistry";
+import { MantineEmotionProvider, emotionTransform } from "@mantine/emotion";
 
 export const metadata = {
   title: "Mantine Next.js template",
@@ -20,7 +21,13 @@ export default function RootLayout({ children }: { children: any }) {
         />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <RootStyleRegistry>
+          <MantineEmotionProvider>
+            <MantineProvider theme={theme} stylesTransform={emotionTransform}>
+              {children}
+            </MantineProvider>
+          </MantineEmotionProvider>
+        </RootStyleRegistry>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import { Box } from "@mantine/core";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <Box
+      sx={(theme, u) => ({
+        padding: 40,
+
+        [u.light]: {
+          backgroundColor: theme.colors.blue[0],
+          color: theme.colors.blue[9],
+
+          "&:hover": {
+            backgroundColor: theme.colors.blue[1],
+          },
+        },
+      })}
+    >
+      Box with emotion sx prop
+    </Box>
+  );
 }
