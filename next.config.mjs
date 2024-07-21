a5bc3530f7710b02e38054119e5ffe6268559a13
Preston Carlton <prescarlton@gmail.com>
2024-07-21 03:56:53 -0400

    Fix warnings (#5)

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 };
 
 export default nextConfig;
