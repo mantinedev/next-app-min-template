d518939620430c1cefd2039262dd9de31e1314b1
Vitaly Rtishchev <rtivital@gmail.com>
2024-10-23 20:07:44 +0400

    Update to Next.js 15

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/favicon.svg" />
